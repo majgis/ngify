3ea27da1c83178d97ebaf21fe3136e64d1d7cb97
majgis <majgis@gmail.com>
2015-04-24 20:14:12 -0600

    two more cases added;tests

--- spec/NgifyJavaScriptStreamReader.spec.js
+++ spec/NgifyJavaScriptStreamReader.spec.js
@@ -80,81 +80,155 @@ describe('NgifyJavaScriptStreamReader', function () {
         })
     });
 
-    it('should produce correct output for provider', function () {
-        executeScenario({
-            code: 'exports["@ng"]={type:"provider", name:"test", inject:["sam"]};',
-            append: "angular.module('ngify').provider('test', [ 'sam', module.exports ] );"
+    describe('jsTemplates', function () {
+        it('should produce correct output for provider', function () {
+            executeScenario({
+                code: 'exports["@ng"]={type:"provider", name:"test", inject:["sam"]};',
+                append: "angular.module('ngify').provider('test', [ 'sam', module.exports ] );"
+            });
+        });
+
+        it('should produce correct output for controller', function () {
+            executeScenario({
+                code: 'exports["@ng"]={type:"controller", name:"test", inject:["sam"]};',
+                append: "angular.module('ngify').controller('test', [ 'sam', module.exports ] );"
+            });
+        });
+
+        it('should produce correct output for factory', function () {
+            executeScenario({
+                code: 'exports["@ng"]={type:"factory", name:"test", inject:["sam"]};',
+                append: "angular.module('ngify').factory('test', [ 'sam', module.exports ] );"
+            });
+        });
+
+        it('should produce correct output for value', function () {
+            executeScenario({
+                code: 'exports["@ng"]={type:"value", name:"test", inject:["sam"]};',
+                append: "angular.module('ngify').value('test', module.exports );"
+            });
+        });
+
+        it('should produce correct output for constant', function () {
+            executeScenario({
+                code: 'exports["@ng"]={type:"constant", name:"test", inject:["sam"]};',
+                append: "angular.module('ngify').constant('test', module.exports );"
+            });
+        });
+
+        it('should produce correct output for animation', function () {
+            executeScenario({
+                code: 'exports["@ng"]={type:"animation", name:"test", inject:["sam"]};',
+                append: "angular.module('ngify').animation('test', [ 'sam', module.exports ] );"
+            });
+        });
+
+        it('should produce correct output for filter', function () {
+            executeScenario({
+                code: 'exports["@ng"]={type:"filter", name:"test", inject:["sam"]};',
+                append: "angular.module('ngify').filter('test', [ 'sam', module.exports ] );"
+            });
+        });
+
+        it('should produce correct output for filter', function () {
+            executeScenario({
+                code: 'exports["@ng"]={type:"filter", name:"test", inject:["sam"]};',
+                append: "angular.module('ngify').filter('test', [ 'sam', module.exports ] );"
+            });
+        });
+
+        it('should produce correct output for directive', function () {
+            executeScenario({
+                code: 'exports["@ng"]={type:"directive", name:"test", inject:["sam"]};',
+                append: "angular.module('ngify').directive('test', [ 'sam', module.exports ] );"
+            });
+        });
+
+        it('should produce correct output for config', function () {
+            executeScenario({
+                code: 'exports["@ng"]={type:"config", name:"test", inject:["sam"]};',
+                append: "angular.module('ngify').config([ 'sam', module.exports ]);"
+            });
+        });
+
+        it('should produce correct output for run', function () {
+            executeScenario({
+                code: 'exports["@ng"]={type:"run", name:"test", inject:["sam"]};',
+                append: "angular.module('ngify').run([ 'sam', module.exports ]);"
+            });
         });
     });
 
-    it('should produce correct output for controller', function () {
-        executeScenario({
-            code: 'exports["@ng"]={type:"controller", name:"test", inject:["sam"]};',
-            append: "angular.module('ngify').controller('test', [ 'sam', module.exports ] );"
+    describe('optional inject annotation by assignment of already declared function ', function () {
+
+        it('should create injectables for a single param', function () {
+            executeScenario({
+                code: 'function test(sam){};exports=module.exports=test;exports["@ng"]={type:"run", name:"test"};',
+                append: "angular.module('ngify').run([ 'sam', module.exports ]);"
+            });
         });
+
+        it('should create injectables for multiple params', function () {
+            executeScenario({
+                code: 'function test(sam, bilbo, frodo){};exports=module.exports=test;exports["@ng"]={type:"run", name:"test"};',
+                append: "angular.module('ngify').run([ 'sam', 'bilbo', 'frodo', module.exports ]);"
+            });
+        });
+
     });
 
-    it('should produce correct output for factory', function () {
-        executeScenario({
-            code: 'exports["@ng"]={type:"factory", name:"test", inject:["sam"]};',
-            append: "angular.module('ngify').factory('test', [ 'sam', module.exports ] );"
+    describe('optional inject annotation by assignment of variable ', function () {
+
+        it('should create injectables for a single param', function () {
+            executeScenario({
+                code: 'var testt = function(sam){};exports=module.exports=testt;exports["@ng"]={type:"run", name:"test"};',
+                append: "angular.module('ngify').run([ 'sam', module.exports ]);"
+            });
         });
+
+        it('should create injectables for multiple params', function () {
+            executeScenario({
+                code: 'var testt = function (sam, bilbo, frodo){};exports=module.exports=testt;exports["@ng"]={type:"run", name:"test"};',
+                append: "angular.module('ngify').run([ 'sam', 'bilbo', 'frodo', module.exports ]);"
+            });
+        });
+
     });
 
-    it('should produce correct output for value', function () {
-        executeScenario({
-            code: 'exports["@ng"]={type:"value", name:"test", inject:["sam"]};',
-            append: "angular.module('ngify').value('test', module.exports );"
+    describe('optional inject annotation by assignment of function expression ', function () {
+
+        it('should create injectables for a single param', function () {
+            executeScenario({
+                code: 'exports=module.exports=function testt(sam){};exports["@ng"]={type:"run", name:"test"};',
+                append: "angular.module('ngify').run([ 'sam', module.exports ]);"
+            });
         });
+
+        it('should create injectables for multiple params', function () {
+            executeScenario({
+                code: 'exports=module.exports=function testt(sam, bilbo, frodo){};exports["@ng"]={type:"run", name:"test"};',
+                append: "angular.module('ngify').run([ 'sam', 'bilbo', 'frodo', module.exports ]);"
+            });
+        });
+
     });
 
-    it('should produce correct output for constant', function () {
-        executeScenario({
-            code: 'exports["@ng"]={type:"constant", name:"test", inject:["sam"]};',
-            append: "angular.module('ngify').constant('test', module.exports );"
-        });
-    });
+    describe('inject annotation trumps optional shorthand', function () {
 
-    it('should produce correct output for animation', function () {
-        executeScenario({
-            code: 'exports["@ng"]={type:"animation", name:"test", inject:["sam"]};',
-            append: "angular.module('ngify').animation('test', [ 'sam', module.exports ] );"
+        it('should create injectables for a single param', function () {
+            executeScenario({
+                code: 'exports=module.exports=function testt(sam){};exports["@ng"]={type:"run", name:"test", inject: ["fred"]};',
+                append: "angular.module('ngify').run([ 'fred', module.exports ]);"
+            });
         });
-    });
 
-    it('should produce correct output for filter', function () {
-        executeScenario({
-            code: 'exports["@ng"]={type:"filter", name:"test", inject:["sam"]};',
-            append: "angular.module('ngify').filter('test', [ 'sam', module.exports ] );"
+        it('should create injectables for multiple params', function () {
+            executeScenario({
+                code: 'exports=module.exports=function testt(sam, bilbo, frodo){};exports["@ng"]={type:"run", name:"test", inject:["a", "b","c"]};',
+                append: "angular.module('ngify').run([ 'a', 'b', 'c', module.exports ]);"
+            });
         });
-    });
 
-    it('should produce correct output for filter', function () {
-        executeScenario({
-            code: 'exports["@ng"]={type:"filter", name:"test", inject:["sam"]};',
-            append: "angular.module('ngify').filter('test', [ 'sam', module.exports ] );"
-        });
-    });
-
-    it('should produce correct output for directive', function () {
-        executeScenario({
-            code: 'exports["@ng"]={type:"directive", name:"test", inject:["sam"]};',
-            append: "angular.module('ngify').directive('test', [ 'sam', module.exports ] );"
-        });
-    });
-
-    it('should produce correct output for config', function () {
-        executeScenario({
-            code: 'exports["@ng"]={type:"config", name:"test", inject:["sam"]};',
-            append: "angular.module('ngify').config([ 'sam', module.exports ]);"
-        });
-    });
-
-    it('should produce correct output for run', function () {
-        executeScenario({
-            code: 'exports["@ng"]={type:"run", name:"test", inject:["sam"]};',
-            append: "angular.module('ngify').run([ 'sam', module.exports ]);"
-        });
     });
 
 });
